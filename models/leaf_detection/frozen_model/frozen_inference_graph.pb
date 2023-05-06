565e692aa1c1a9d58e2f819d013f0694028c22bf
amine <aminembarek32@gmail.com>
2023-05-06 11:07:49 +0200

    add leaf detection

---
+++ models/leaf_detection/frozen_model/frozen_inference_graph.pb
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5e0f84f14532b6190e7af320b715dd877cf782268fa319e71c792a8c08d48694
+size 19097629
--- test_app.py
+++ test_app.py
@@ -5,22 +5,22 @@ from tensorflow import keras
 import numpy as np
 from PIL import Image
 name_img = "leaf_detection.jpg"
-#image = keras.utils.load_img("/Users/aminembarek/Desktop/app/test/tomato.jpeg", target_size=(448, 448))
-#Serialized = pickle.dumps(image, protocol=pickle.DEFAULT_PROTOCOL)
-#files = {'image': (name_img, Serialized, 'multipart/form-data', {'Expires': '0'})}
-#resp = requests.post('https://leafdetection-kjlyzrfelq-og.a.run.app',files = files)
-#image= pickle.loads(resp.content)
-#
-#plt.figure()
-#plt.imshow(image)
-#plt.show()
-#image = Image.fromarray(image)
-#image = image.resize((224,224))
+image = keras.utils.load_img("/Users/aminembarek/Desktop/app/test/tomato.jpeg")
+Serialized = pickle.dumps(image, protocol=pickle.DEFAULT_PROTOCOL)
+files = {'image': (name_img, Serialized, 'multipart/form-data', {'Expires': '0'})}
+resp = requests.post('http://127.0.0.1:8000/leaf_detection',files = files)
+image= pickle.loads(resp.content)
+
+plt.figure()
+plt.imshow(image)
+plt.show()
+image = Image.fromarray(image)
+image = image.resize((224,224))
 name_img = "disease_detection.jpg"
 image = keras.utils.load_img("/Users/aminembarek/Desktop/app/test/TomatoEarlyBlight3.JPG", target_size=(224, 224))
 Serialized = pickle.dumps(image, protocol=pickle.DEFAULT_PROTOCOL)
 files = {'image': (name_img, Serialized, 'multipart/form-data', {'Expires': '0'})}
-resp = requests.post('https://ld-kjlyzrfelq-og.a.run.app',files = files)
+resp = requests.post('http://127.0.0.1:8000/leaf_disease',files = files)
 print(resp.json())
 result = resp.json()
 
